fdbebebbcfdaf3fb4a10115b6c15774d0f364f4f
OlliePryde <olliepryde@gmail.com>
2024-08-16 21:09:11 +0100

    initial setup commit

---
+++ src/main/java/com/mendmend/MendingMending.java
@@ -0,0 +1,14 @@
+package com.mendmend;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MendingMending implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("mending-mending");
+
+	@Override
+	public void onInitialize() { }
+}
